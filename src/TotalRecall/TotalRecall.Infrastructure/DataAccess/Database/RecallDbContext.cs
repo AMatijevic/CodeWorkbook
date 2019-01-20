d662064f6814d86932dda82c2952cca58daf4c3a
Andrej Matijevic <Andrej.Matijevic@dreamlines.de>
2019-01-20 09:00:28 +0100

    Add empty DBContext and libraries for SQLite

---
+++ src/TotalRecall/TotalRecall.Infrastructure/DataAccess/Database/RecallDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TotalRecall.Infrastructure.DataAccess.Database
+{
+    public class RecallDbContext : DbContext
+    {
+        public RecallDbContext(DbContextOptions<RecallDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
